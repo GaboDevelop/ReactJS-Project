ef874f79f141e3d45d44c4221ef88cc6a7d653d1
Oscar Velasquez <ovelasquez@option.cl>
2019-04-12 07:54:27 -0400

    dependencias 2

---
+++ src/components/Badge.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+import "./styles/Badge.css";
+import confLogo from "../images/badge-header.svg";
+
+//Todo componente es una clase y extiende de React.Component
+class Badge extends React.Component {
+  //siempre debe tener el metodo render
+  render() {
+    return (
+      //usar className , cambiando el csss clasico para evitar un error de JS en consola
+      <div className="Badge">
+        <div className="Badge__header">
+          <img src={confLogo} alt="Logo de conferencia" />
+        </div>
+
+        <div className="Badge__section-name">
+          <img
+            className="Badge__avatar"
+            src={this.props.avatarUrl}
+            alt="Avatar"
+          />
+          <h1>
+            {this.props.firstname}
+            <br />
+            {this.props.lastname}
+          </h1>
+        </div>
+
+        <div className="Badge__section-info">
+          <h3>{this.props.jobTitle}</h3>
+          <div>@{this.props.twitter}</div>
+        </div>
+
+        <div className="Badge__footer">#GaboDevelop</div>
+      </div>
+    );
+  }
+}
+
+//Siempre se debe exportar el componente
+export default Badge;
---
+++ src/index.js
@@ -0,0 +1,56 @@
+//ASI SERIA CON UN JS NORMAL
+/*const element = document.createElement("h1");
+element.innerHTML = " Hello , plat Badges!";
+
+const container = document.getElementById("app");
+
+container.appendChild(element);*/
+
+//AHORA DE LA MANERA REACT CON REACTDOM
+
+import React from "react"; //
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
+
+//importando un componente
+import Badge from "./components/Badge";
+//Importando estilos globales
+import "./global.css";
+
+//const jsx = <h1>Hello , plat desde Reac</h1>; //esto es gracias a JSX
+
+//jsx y elemnt equivalen a lo mismo solo que uno es JSX y otro es componente de react
+
+//                                   etiqueta,atributo,informacion o contenido
+/*const element = React.createElement(
+  "a",
+  {
+    href: "https://googe.com"
+  },
+  "ir a Google"
+);*/
+//const name = "gabish";
+//const element = React.createElement("h1", {}, `hola soy ${name}`);
+//const jsx = <h1>Holas soy {name}</h1>;
+
+/*const jsx = (
+  <div>
+    <h1>Hola soy {name + " Ortega"}</h1>
+  </div>
+);*/
+
+const container = document.getElementById("app");
+
+//ReactDOM.render(_que_ , _donde_)
+//ReactDOM.render(element, container);
+//ReactDOM.render(jsx, container);
+ReactDOM.render(
+  <Badge
+    firstname="Lilly"
+    avatarUrl="https://s.gravatar.com/avatar/81093cc55c7768e60d3f6138098f6902?s=80"
+    lastname="Ortega"
+    jobTitle="FrontEnd Developer"
+    twitter="elgabish"
+  />,
+  container
+);
